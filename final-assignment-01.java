a49ead10f90424f054e29764fd05105f5f9618ea
Ruwanthi Yashodara <119606787+yashodar23NEW@users.noreply.github.com>
2024-01-02 10:43:15 +0530

    Create final-assignment-01.java

---
+++ final-assignment-01.java
@@ -0,0 +1,9 @@
+package com.mycompany.javafinal;
+
+
+       public class Javafinal {
+
+        public static void main(String[] args) {
+        System.out.println("My name is Ruwanthi Yashodara");
+    }
+}
